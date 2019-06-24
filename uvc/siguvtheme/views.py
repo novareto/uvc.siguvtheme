8d63e1470b226740c4ecdc19a53790ae4333f5b3
Christian Klinger <ck@novareto.de>
2019-06-24 16:01:05 +0200

    start to have a bootstrap based form

---
+++ uvc/siguvtheme/views.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# # Copyright (c) 2007-2019 NovaReto GmbH
+# # cklinger@novareto.de
+
+import grok
+import megrok.pagetemplate as pt
+
+
+from .skin import ISiguvTheme
+from zope.interface import Interface
+from grokcore.chameleon.components import ChameleonPageTemplateFile
+
+
+grok.templatedir("templates")
+
+
+class FormMacros(grok.View):
+    grok.context(Interface)
+    template = ChameleonPageTemplateFile("templates/formtemplate.cpt")
+
+
+class FieldMacros(grok.View):
+    grok.context(Interface)
+    template = ChameleonPageTemplateFile("templates/fieldtemplates.cpt")
+
+
+class FormTemplate(pt.PageTemplate):
+    grok.layer(ISiguvTheme)
