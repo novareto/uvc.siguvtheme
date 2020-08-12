ecd23860c317a499b936d2f49b4310919667a8c6
Christian Klinger <ck@novareto.de>
2020-08-12 11:43:13 +0200

    changes

--- uvc/siguvtheme/resources.py
+++ uvc/siguvtheme/resources.py
@@ -8,15 +8,20 @@ from js.fontawesome import fontawesome_all_css
 
 library = Library('uvc.siguvtheme', 'static')
 
-bootstrap_css = Resource(
-    library, 'bootstrap.css',
-    compiler="sass",
-    source='siguvtheme.scss'
-)
+#bootstrap_css = Resource(
+#    library, 'bootstrap.css',
+#    compiler="sass",
+#    source='siguvtheme.scss'
+#)
+#
+#bootstrap_js = Resource(
+#    library, 'bootstrap-4.3.1/dist/js/bootstrap.bundle.min.js', depends=[jquery, ] 
+#)
 
-bootstrap_js = Resource(
-    library, 'bootstrap-4.3.1/dist/js/bootstrap.bundle.min.js', depends=[jquery, ] 
-)
+sidebar_css = Resource(library, 'sidebar.css')
 
-main_css = Resource(library, 'main.css', depends=[bootstrap_css, fontawesome_all_css ])
+bootstrap_css = Resource(library, 'uvcsiguvtheme.css', compiler="sass", source="scss/siguv.scss")
+bootstrap_js = Resource(library, 'bootstrap.bundle.js',  depends=[jquery, ],  bottom=True)
+
+main_css = Resource(library, 'main.css', depends=[bootstrap_css, fontawesome_all_css, sidebar_css ])
 main_js = Resource(library, 'main.js', depends=[bootstrap_js, ])
--- uvc/siguvtheme/static/main.js
+++ uvc/siguvtheme/static/main.js
@@ -12,3 +12,64 @@ $(document).ready(function() {
 				$("body").attr("data-with-sidebar", "")
 		})
 })
+$(document).ready(function () {
+
+  // Custom JavaScript
+  function nearTo($element, distance, side, event) {
+    var left = $element.offset().left;
+    var loffset = left + distance;
+    var right = left + $element.width();
+    var roffset = right - distance;
+    var x = event.pageX;
+    if (side == 'left') {
+      return x < loffset;
+    };
+    if (side == 'right') {
+      return x > roffset;
+    };
+  };
+
+  function showSidebar() {
+    $('#portal-navigation-cover').fadeIn('500');
+    $('body').attr('data-with-sidebar', 'true');    
+  }
+
+  function hideSidebar() {
+    $('#portal-navigation-cover').fadeOut('500');
+    $('body').attr('data-with-sidebar', 'false');
+  }
+
+
+    // Mouse
+    $('body').mousemove(function (event) {
+      var nav = $('#portal-navigation');
+      var offset = 30;
+      if (nav.hasClass('sidebar-left')) {
+        var nearby = nearTo(nav, offset, 'left', event);
+        if (nearby) {
+          showSidebar();
+        };
+      } else {
+        var nearby = nearTo(nav, offset, 'right', event);
+        if (nearby) {
+          showSidebar();
+        };
+      };
+    });
+
+    // Burger
+
+
+    $('#portal-navigation-cover').click(function (e) {
+      e.preventDefault();
+      hideSidebar();
+    });
+
+
+
+  $('#sidebar-handle').click(function (e) {
+    e.preventDefault();
+    showSidebar();
+  });
+
+});
