512c3f2832511927723fab33b26f09928336eed8
Christian Klinger <ck@novareto.de>
2023-11-27 09:26:05 +0100

    Changes

--- uvc/siguvtheme/static/main.js
+++ uvc/siguvtheme/static/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	$("#portal-globalnav").prepend('<li id="portaltab-burger-menu"><i class="glyphicon glyphicon-menu-hamburger" /></li>'),
+        /*
 		$("body").on("mousemove", function(e) {
 			e.pageX < 20 && $("body").attr("data-with-sidebar", "true")
 		}),
+        */
 		$("#portaltab-burger-menu").click(function(e) {
 			e.preventDefault(),
 				$("body").attr("data-with-sidebar", "true")
